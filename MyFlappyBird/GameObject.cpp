6367077d34e2ae58d2f75d93766b76ccdae35c12
EVGENIOS KRIMITSAS <99003397+krimits@users.noreply.github.com>
2024-02-01 10:16:33 +0200

    Update GameObject.cpp

--- MyFlappyBird/GameObject.cpp
+++ MyFlappyBird/GameObject.cpp
@@ -3,8 +3,27 @@
 
 int GameObject::m_next_id = 0;
 
-GameObject::GameObject(GameState* gs, const std::string& name)
-	: m_state(*gs), m_name(name), m_id(m_next_id++), m_active(true)
-{
-	// Constructor implementation
+GameObject::GameObject(GameState& gs, const std::string& name)
+    : m_state(gs), m_name(name), m_id(m_next_id++), m_active(true) {
+    // Αρχικοποίηση του αντικειμένου
 }
+
+GameObject::GameObject(GameState* gs, const std::string& name)
+    : m_state(*gs), m_name(name), m_id(m_next_id++), m_active(true) {
+    // Αρχικοποίηση του αντικειμένου με δείκτη GameState
+    // Εξασφαλίστε ότι το gs δεν είναι nullptr πριν το ακολουθήσετε
+}
+
+GameObject::~GameObject() {
+    // Καταστροφέας - καθαρισμός πόρων, εάν χρειάζεται
+}
+
+bool GameObject::isActive() const {
+    return m_active;
+}
+
+void GameObject::setActive(bool active) {
+    m_active = active;
+}
+
+// Υπόλοιπες μέθοδοι και λογική
