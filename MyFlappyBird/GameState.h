d517494484e6b3302f956a4c5556fc692826004f
EVGENIOS KRIMITSAS <99003397+krimits@users.noreply.github.com>
2024-02-01 09:53:20 +0200

    Update GameState.h

--- MyFlappyBird/GameState.h
+++ MyFlappyBird/GameState.h
@@ -2,7 +2,48 @@
 
 #include "player.h"
 #include "Pipe.h"
+#include "Movi#pragma once
+
+#include <memory>
+#include "Player.h"
+#include "Pipe.h"
 #include "MovingPipe.h"
+
+// Κλάση GameState για τη διαχείριση της κατάστασης του παιχνιδιού
+class GameState {
+    typedef enum { START, PLAY, END } status_g; // Καταστάσεις του παιχνιδιού
+
+    std::unique_ptr<Player> player; // Έξυπνος δείκτης για τον παίκτη
+    std::unique_ptr<Pipe> pipe; // Έξυπνος δείκτης για το εμπόδιο
+    bool withinX;
+    bool withinY;
+    bool withinX_old;
+    bool collision; // Μεταβλητή για τον έλεγχο σύγκρουσης
+    int scoreCounter = 0; // Μετρητής σκορ
+    int highScore = 0; // Υψηλότερο σκορ
+    int difficulty = 1; // Επίπεδο δυσκολίας
+    status_g status; // Τρέχουσα κατάσταση του παιχνιδιού
+
+    void spawnPipe(); // Δημιουργία εμποδίου
+    bool checkCollision(); // Έλεγχος για σύγκρουση
+    void updateStart(); // Ενημέρωση κατάστασης START
+    void updatePlay(); // Ενημέρωση κατάστασης PLAY
+    void updateEnd(); // Ενημέρωση κατάστασης END
+    void drawStart(); // Σχεδίαση κατάστασης START
+    void drawPlay(); // Σχεδίαση κατάστασης PLAY
+    void drawEnd(); // Σχεδίαση κατάστασης END
+
+public:
+    GameState();
+    ~GameState();
+    void update(); // Ενημέρωση της κατάστασης του παιχνιδιού
+    void draw(); // Σχεδίαση του παιχνιδιού
+    void init(); // Αρχικοποίηση της κατάστασης του παιχνιδιού
+	
+    void increaseDifficulty() { difficulty++; } // Αύξηση της δυσκολίας
+    int getDifficulty() const { return difficulty; } // Λήψη του επιπέδου δυσκολίας
+};
+ngPipe.h"
 
 class GameState
 {
